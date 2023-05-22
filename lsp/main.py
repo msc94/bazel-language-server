7c24423c1f4a597e7dcdb51e5acda5bc6b59724b
msc94 <msc94@users.noreply.github.com>
2023-05-22 21:46:04 +0200

    Add skeleton

--- lsp/main.py
+++ lsp/main.py
@@ -1,9 +1,34 @@
 import logging
 
+from lsprotocol.types import (
+    TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_DEFINITION,
+    CompletionItem,
+    CompletionList,
+    CompletionParams,
+    Definition,
+    DefinitionParams,
+    Location,
+)
+from pygls.server import LanguageServer
 
-def main():
-    logging.info("Starting Bazel LSP")
+logging.basicConfig(level=logging.DEBUG)
+server = LanguageServer("bazel-language-server", "v0.1")
 
 
-if __name__ == "__main__":
-    main()
+# https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_definition
+@server.feature(TEXT_DOCUMENT_DEFINITION)
+def definitions(params: DefinitionParams) -> Definition:
+    logging.info("Handling definition request")
+
+    document = server.workspace.get_document(params.text_document.uri)
+    logging.info(f"Document: {document}")
+
+    current_line = document.lines[params.position.line].strip()
+    logging.debug(f"Current line: {current_line}")
+
+    return None
+
+
+logging.info("Starting bazel LSP")
+server.start_io()
